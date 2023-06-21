c004a60c87d044c7ad580720c2d80eb54f3dc5b1
NozoDev <kevinroj326@gmail.com>
2023-06-21 13:59:18 -0500

    2 tarea de motors rutas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,54 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const sanitizer = require('perfect-express-sanitizer');
 
+//errores
+const AppError = require('./utils/appError');
+const handlerError = require('./controllers/error.controller');
 
+//rutas
 const usersRouter = require('./routes/users.routes');
 const repairsRouter = require('./routes/repairs.routes');
 
 const app = express();
+const limiter = rateLimit({
+  max: 100000,
+  windowMs: 60 * 60 * 5000,
+  message: 'Too many requests from this IP, please try again in one hour',
+});
+
+//middlewares
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+app.use(hpp());
+app.use(
+  sanitizer.clean({
+    xss: true,
+    noSql: true,
+    sql: true,
+  })
+);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('api/v1', limiter);
+
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/repairs', repairsRouter);
+
+// errors
+app.all('*', (req, res, next) => {
+  return next(
+    new AppError(`Cant find ${req.originalUrl} on this server!`, 404)
+  );
+});
 
 app.use(express.json());
 
@@ -15,7 +59,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/v1/users', usersRouter);
-app.use('/api/v1/repairs', repairsRouter);
+app.use(handlerError);
 
 module.exports = app;
